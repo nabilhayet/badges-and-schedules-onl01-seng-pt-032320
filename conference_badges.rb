d6478660d27d795301391e056dd34af1decba807
Nabil Hayet <nabil.bracu@gmail.com>
2021-06-23 14:02:52 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,28 +4,30 @@ end
 
 def batch_badge_creator(attendees)
  ss=Array.new
- attendees.collect do |person|
- ss << "Hello, my name is #{person}."
- end
+  attendees.collect do |person|
+   ss << "Hello, my name is #{person}."
+  end
  return ss
  end
+ 
 def assign_rooms(attendees)
   sss=Array.new
   attendees.each_with_index do |attendee,index| 
-  sss << "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
-end
-return sss
+   sss << "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
+  end
+ return sss
 end
 
 def printer(attendees)
-    res1=batch_badge_creator(attendees)
-    res1.each do |person|
-      puts person
-end
+   res1=batch_badge_creator(attendees)
+   res1.each do |person|
+    puts person
+   end
+   
    res2=assign_rooms(attendees)
-    res2.each do |person|
-      puts person
-end 
+   res2.each do |person|
+    puts person
+   end 
 end 
 
 
